8fce045e145e3bab5783d16d8221a1c60d0546c6
heaven78 <fixer90210@gmail.com>
2018-05-19 14:54:14 -0500

    el controlador regresa un Entero 45 a al vista

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace WebApplication1.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View("Index",45);
         }
     }
 }
